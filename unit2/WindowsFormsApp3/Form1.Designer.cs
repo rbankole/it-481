9c5e538005d03b63ee299f090964c7d636772966
cha0sm0nk3y <olukayode.bankole@kaplan.edu>
2021-03-27 16:45:40 -0400

    add unit2

---
+++ unit2/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(256, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(106, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(394, 289);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input Options";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(256, 56);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Customer LastNames";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 227);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(256, 56);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Number of Customers";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ unit2/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cnn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        string output = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            cnn = new SqlConnection("Data Source=DESKTOP-RMMFSO1;Initial Catalog=Northwind;Integrated Security=True");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            connetionString = @"Data Source=DESKTOP-RMMFSO1;Initial Catalog=Northwind;Integrated Security=True";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            MessageBox.Show("Connection to DB is Open!");
+            cnn.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand();
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandText = "SELECT [ContactName] FROM[Northwind].[dbo].[Customers]";
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                output = output + dr.GetValue(0) + "\n";
+            }
+            MessageBox.Show(output);
+            cnn.Close();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand();
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandText = "SELECT COUNT(*) FROM[Northwind].[dbo].[Customers]";
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                output = output + dr.GetValue(0) + "\n";
+            }
+            MessageBox.Show("Number of customers is " + output);
+            cnn.Close();
+        }
+    }
+}
